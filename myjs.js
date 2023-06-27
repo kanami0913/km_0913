5f377794139f3d08f938d24614802c07bde5df23
kanami0913 <131091876+kanami0913@users.noreply.github.com>
2023-06-27 09:22:49 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -6,4 +6,8 @@ alert(Math.sqrt(2+3));
 alert('2'+'3');
 alert(Math.sqrt('2'+'3'));
 alert(Number('2')+Number('3'));
-
+//alert(Math.aqrt(Number('2')+Number('3')));
+alert('2'+3);
+alert(Math.sqrt('2'+3);
+alert(Number('2')+3);
+alert(Math.sqrt(Number('2')+3));
