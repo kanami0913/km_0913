8f993a3a0f3c4c0b199d41fac0e7be1d89deab71
kanami0913 <131091876+kanami0913@users.noreply.github.com>
2023-07-04 10:04:26 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,7 +1,10 @@
 alert('Hello,Kanami!');
 alert(1+1);
 alert(Math.sqrt(1+1));
-alert(2+3);
+alert(Math.sqrt(2+3));
+alert(1/0)
+alert(0/0)
+alert(2+3)
 alert('2'+'3');
 alert(Number('2')+Number('3'));
 alert('2'+3);
